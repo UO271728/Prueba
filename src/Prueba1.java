8f184e120be0173a6c3d57d1b5d4461eaa4cd21f
Rafa <Rafa@DESKTOP-94V6AQD>
2020-10-18 23:31:16 +0200

    cambio y borrado

--- src/Prueba1.java
+++ src/Prueba1.java
@@ -2,7 +2,7 @@
 public class Prueba1 {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("linea 1");
-		System.out.println("linea 1");
+		System.out.println("linea 2");
+		System.out.println("linea 3");
 	}
 }
